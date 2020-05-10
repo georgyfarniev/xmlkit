5256f4414da0eea46c29c3d840b4b35dbd105c78
Georgy Farniev <gfarniev@icloud.com>
2020-05-10 13:57:44 +0400

    add basic test and debug setup

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  "transform": {
-    "^.+\\.spec\\.ts$": "ts-jest"
-  },
-  "moduleFileExtensions": ["ts", "js"],
-  "preset": "ts-jest/presets/js-with-ts",
+  // "transform": {
+  //   "^.+\\.spec\\.ts$": "ts-jest"
+  // },
+  // "moduleFileExtensions": ["ts", "js"],
+  // "preset": "ts-jest/presets/js-with-ts",
   "testEnvironment": "node",
   "testMatch": [
     "**/tests/**/*.spec.ts"
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,6 +1,9 @@
 import 'jest'
+import { getMeaningOfLife } from '../'
 
 describe('Index tests', () => {
   test('test', async () => {
+    const ret = getMeaningOfLife()
+    expect(ret).toBe(42)
   })
 })
