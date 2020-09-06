fc7297c0228f9f5b783d5fba7b11552f4f064489
Georgy Farniev <gfarniev@icloud.com>
2020-09-06 15:33:08 +0400

    cleanup

--- src/xml_sax.ts
+++ src/xml_sax.ts
@@ -1,23 +1,13 @@
 import sax from 'sax'
 import { EventEmitter } from 'events'
 
-interface Attrs {
-  [k: string]: string
-}
-
-export interface Tag {
-  name: string
-  attrs: Attrs
-  isSelfClosing: boolean
-}
-
 // Abstraction class to hide sax module behind and allow easy replacement
 export class XmlSax extends EventEmitter {
   constructor(private parser = sax.parser(true)) {
     super()
 
-    parser.onopentag = ({ ns, isSelfClosing, name, attributes}: any) =>
-      this.emit('opentag', { ns, name, isSelfClosing, attrs: attributes })
+    parser.onopentag = ({  isSelfClosing, name, attributes }) =>
+      this.emit('opentag', {  name, isSelfClosing, attrs: attributes })
 
     parser.onclosetag = (name: string) => this.emit('closetag', name)
     parser.ontext = (text: string) => this.emit('text', text)
